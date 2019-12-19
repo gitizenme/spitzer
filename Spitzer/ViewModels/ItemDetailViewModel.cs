b2c7c38c0fb5ff5ecdba3341599587c7718d5dd3
Joe Chavez <shop@chavez.ws>
2019-12-18 16:53:43 -0800

    #6 Add cached image renderer, display image dimensions in list of available images. Add image metadata.

--- Spitzer/ViewModels/ItemDetailViewModel.cs
+++ Spitzer/ViewModels/ItemDetailViewModel.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FFImageLoading.Forms;
 using Spitzer.Models;
 using Spitzer.Models.ImageMetadata;
+using Xamarin.Forms;
 
 namespace Spitzer.ViewModels
 {
     public class ItemDetailViewModel : BaseViewModel
     {
-        public IList<Uri> Images { get; }
+        public IList<Uri> ImageUriList { get; }
+        public IDictionary<string, StackLayout> ImageLayout { get; }
         public ImageMetaData Metadata { get; }
         public MediaItem Item { get; set; }
         public ItemDetailViewModel(MediaItem item = null)
         {
+            ImageLayout = new Dictionary<string, StackLayout>();
             Title = item.Title;
-            Images = item.Images;
+            ImageUriList = item.Images;
             Metadata = item.MetaData;
             Item = item;
         }
     }
+
 }
--- Spitzer/Views/ItemDetailPage.xaml.cs
+++ Spitzer/Views/ItemDetailPage.xaml.cs
@@ -2,11 +2,16 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 using Spitzer.Models;
 using Spitzer.ViewModels;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using FFImageLoading.Forms;
+using FFImageLoading.Mock;
+using FFImageLoading.Work;
 using Spitzer.Models.ImageMetadata;
+using Xamarin.Essentials;
 
 namespace Spitzer.Views
 {
@@ -47,52 +52,79 @@ namespace Spitzer.Views
         {
             var divider = new BoxView {BackgroundColor = Color.Black, HeightRequest = 3};
 
-            foreach (var image in viewModel.Images)
+            foreach (var imageUri in viewModel.ImageUriList)
             {
-                if (image.ToString().EndsWith(".jpg", StringComparison.Ordinal))
+                if (imageUri.ToString().EndsWith(".jpg", StringComparison.Ordinal))
                 {
-                    Images.Children.Add(divider);
-                    var imageView = new Image
-                        {Source = image, HeightRequest = 50, WidthRequest = 50, HorizontalOptions = LayoutOptions.Start};
-
-                    var imageWithLabelLayout = new StackLayout {Orientation = StackOrientation.Horizontal};
                     var labelText = "Default";
-                    if (image.PathAndQuery.Contains("~orig"))
+                    if (imageUri.PathAndQuery.Contains("~orig"))
                     {
                         labelText = $"Original Resolution";
                     }
-                    else if (image.PathAndQuery.Contains("~large"))
+                    else if (imageUri.PathAndQuery.Contains("~large"))
                     {
                         labelText = "Large";
                     }
-                    else if (image.PathAndQuery.Contains("~medium"))
+                    else if (imageUri.PathAndQuery.Contains("~medium"))
                     {
                         labelText = "Medium";
                     }
-                    else if (image.PathAndQuery.Contains("~small"))
+                    else if (imageUri.PathAndQuery.Contains("~small"))
                     {
                         labelText = "Small";
                     }
-                    else if (image.PathAndQuery.Contains("~thumb"))
+                    else if (imageUri.PathAndQuery.Contains("~thumb"))
                     {
                         labelText = "Thumbnail";
                     }
 
-                    var imageLabel = new Label {Text = $"{labelText} ({imageView.Width}x{imageView.Height})"};
+                    var imageLabel = new Label
+                    {
+                        Text = $"{labelText}", VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Start
+                    };
+                    var imageView = new CachedImage
+                    {
+                        Source = imageUri, HeightRequest = 50, WidthRequest = 50,
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Start
+                    };
+
+                    var imageWithLabelLayout = new StackLayout {Orientation = StackOrientation.Horizontal};
+                    imageView.Success += (sender, e) =>
+                    {
+                        var imageDimensionsLabel = new Label
+                        {
+                            Text = $"({e.ImageInformation.OriginalHeight}x{e.ImageInformation.OriginalHeight})",
+                            VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Start
+                        };
+                        if (viewModel.ImageLayout.TryGetValue(e.ImageInformation.Path, out var layout))
+                        {
+                            MainThread.BeginInvokeOnMainThread ( () => {
+                                layout.Children.Add(imageDimensionsLabel);
+                                layout.ForceLayout();
+                            });                            
+                        }
+                    };
+
+                    viewModel.ImageLayout[imageUri.ToString()] = imageWithLabelLayout;
+
+                    Images.Children.Add(divider);
                     imageWithLabelLayout.Children.Add(imageView);
                     imageWithLabelLayout.Children.Add(imageLabel);
-                    
                     Images.Children.Add(imageWithLabelLayout);
-                }
-            }
-            var metadata = viewModel.Metadata;
-            if (metadata != null)
-            {
-                Images.Children.Add(divider);
-                foreach(var prop in metadata.GetType().GetProperties()) {
-                    var metadataLine = $"{prop.Name}: {prop.GetValue(metadata, null)}";
-                    Console.WriteLine(metadataLine);
-                    Metadata.Children.Add(new Label {Text = metadataLine});
+
+                    var metadata = viewModel.Metadata;
+                    if (metadata != null)
+                    {
+                        Images.Children.Add(divider);
+                        foreach (var prop in metadata.GetType().GetProperties())
+                        {
+                            var metadataLine = $"{prop.Name}: {prop.GetValue(metadata, null)}";
+                            Debug.WriteLine(metadataLine);
+                            Metadata.Children.Add(new Label {Text = metadataLine});
+                        }
+                    }
                 }
             }
         }
